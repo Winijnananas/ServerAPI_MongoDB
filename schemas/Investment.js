e6341d26a507a746715e6c150effcebb244edc27
Winijnananas <winijnan.ji@mail.wu.ac.th>
2023-04-02 00:48:47 +0700

    payment insert

--- schemas/Investment.js
+++ schemas/Investment.js
@@ -7,6 +7,12 @@ const InvestSchema = new Schema({
   cost: Number,
   type: String,
   status: String,
+  payment: {
+    spendings: [{
+      spendthing: String,
+      costvalue: Number
+    }]
+  },
   startdate: { type: Date, default: Date.now },
   enddate: { type: Date }
 }, { timestamps: true, versionKey: false });
